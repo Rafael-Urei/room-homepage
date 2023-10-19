a08b34e941439650608bf516e09aef6207dcc472
Rafael-Urei <urei.dev@outlook.com>
2023-10-19 17:56:20 -0300

    Finished the fetch data

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,17 @@
+"use client";
+
 import Image from "next/image";
+import useSWR from "swr";
+import Loading from "./components/Loading";
+
+const fetcher = (url: RequestInfo | URL) =>
+  fetch(url).then((res) => res.json());
 
 export default function Home() {
+  const { data, error } = useSWR("/api", fetcher);
+  console.log(data);
+
+  if (!data) return <Loading />;
+
   return <main className="min-h-screen"></main>;
 }
