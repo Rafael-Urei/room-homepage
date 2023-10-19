830b22e0d9c8595f9c7984954a507d44522485e2
Rafael-Urei <urei.dev@outlook.com>
2023-10-19 18:06:16 -0300

    Created the header component.

--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,10 +3,26 @@ import { Logo } from "./Icons";
 
 export default function Header() {
   return (
-    <header className="sticky">
+    <header className="fixed w-full h-28 px-14 flex gap-8 items-center bg-black">
       <Link href="#header">
         <Logo />
       </Link>
+      <nav>
+        <ul className="flex gap-6 text-white">
+          <li>
+            <Link href="#">home</Link>
+          </li>
+          <li>
+            <Link href="#">shop</Link>
+          </li>
+          <li>
+            <Link href="#">about</Link>
+          </li>
+          <li>
+            <Link href="#">contact</Link>
+          </li>
+        </ul>
+      </nav>
     </header>
   );
 }
